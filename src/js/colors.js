382eba7054827a27ac2afb5851706a9e82d9dc7d
russellgoldenberg <russellgoldenberg@gmail.com>
2018-02-28 20:42:35 -0500

    setup

---
+++ src/js/colors.js
@@ -0,0 +1,16 @@
+export default {
+	default: '#dedede',
+	all: '#4a4a4a',
+	categorical: [
+		'#fff',
+		'#66c2a5',
+		'#fc8d62',
+		'#8da0cb',
+		'#e78ac3',
+		'#a6d854',
+		'#ffd92f',
+		'#e5c494',
+		'#b3b3b3',
+		'#01665e'
+	]
+};
---
+++ src/js/trailers.js
@@ -0,0 +1,11 @@
+export default {
+	Dunkirk: 'F-eMt3SrfFU',
+	'Get Out': 'sRfnevzM9kQ',
+	'Call Me by Your Name': 'Z9AYPxH5NTM',
+	'The Shape of Water': 'XFYWazblaUA',
+	'Lady Bird': 'cNi_HC839Wo',
+	'Darkest Hour': 'LtJ60u7SUSw',
+	'Three Billboards': 'Jit3YhGx5pU',
+	'Phantom Thread': 'xNsiQMeSvMk',
+	'The Post': 'nrXlY6gzTTM'
+};
